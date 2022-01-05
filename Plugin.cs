6ec5fb8ac10f825a77baa35d85eb0aee442b924f
strategineer <me@strategineer.com>
2022-01-05 17:47:08 -0500

    f

--- Plugin.cs
+++ Plugin.cs
@@ -3,81 +3,89 @@ using System.Diagnostics;
 using System;
 using UnityEngine;
 using BepInEx.Configuration;
-using MirrorInternalLogs;
 using HarmonyLib;
 using System.Reflection;
 
 namespace com.strategineer.PEBSpeedrunTools
 {
+    class TextGUI
+    {
+        const int SCREEN_OFFSET = 10;
+
+        private string _text;
+        private GUIStyle _style = new GUIStyle();
+        private GUIStyle _shadowStyle = new GUIStyle();
+        private static Rect _rect;
+        private static Rect _shadowRect;
+
+        private static TextAnchor _anchor;
+        public TextGUI() : this(TextAnchor.UpperLeft, Color.yellow, "") { }
+        public TextGUI(TextAnchor anchor, Color color, string text)
+        {
+            _text = text;
+            _anchor = anchor;
+
+            int w = Screen.width, h = Screen.height;
+            _rect = _shadowRect = new Rect(SCREEN_OFFSET, SCREEN_OFFSET, w - SCREEN_OFFSET * 2, h - SCREEN_OFFSET * 2);
+            _shadowRect.x += 3;
+            _shadowRect.y += 3;
+
+            _style.fontSize = h / 40;
+            SetAnchor(anchor);
+            SetColor(color);
+            SetColor(Color.black);
+        }
+
+        public void SetColor(Color color)
+        {
+            _style.normal.textColor = color;
+        }
+        public void SetShadowColor(Color color)
+        {
+            _shadowStyle.normal.textColor = color;
+        }
+
+        public void SetAnchor(TextAnchor anchor)
+        {
+            _style.alignment = anchor;
+        }
+
+        public void SetText(string text)
+        {
+            this._text = text;
+        }
+
+        public void Draw()
+        {
+            GUI.Label(_rect, _text, _style);
+            GUI.Label(_shadowRect, _text, _shadowStyle);
+        }
+    }
 
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInProcess("pigEatBallGame.exe")]
     [HarmonyPatch]
     public class Plugin : BaseUnityPlugin
     {
-
-        public static string[][] worldLevels = new string[][]
-    {
-        new string[]
-        {
-            "tennis_courts_a.prd",
-            "tennis_courts_b.prd",
-            "tennis_courts_c.prd",
-            "tennis_courts_d.prd"
-        },
-        new string[]
-        {
-            "sushi_gardens_a.prd",
-            "sushi_gardens_b.prd",
-            "sushi_gardens_c.prd",
-            "sushi_gardens_d.prd"
-        },
-        new string[]
-        {
-            "sports_barena_a.prd",
-            "sports_barena_b.prd",
-            "sports_barena_c.prd",
-            "sports_barena_d.prd"
-        },
-        new string[]
-        {
-            "kitchen_chaos_alpha.prd",
-            "kitchen_chaos_beta.prd",
-            "kitchen_chaos_gamma.prd",
-            "kitchen_chaos_main_hub.prd"
-        },
-        new string[]
-        {
-            "astro_farm_alpha.prd",
-            "astro_farm_beta.prd",
-            "astro_farm_gamma.prd",
-            "astro_farm_main.prd"
-        }
-    };
-
         private static Harmony h;
         private static Stopwatch stopWatch = new Stopwatch();
         private static ConfigEntry<TextAnchor> _timerPosition;
         private static ConfigEntry<TextAnchor> _debugMsgPosition;
         private static ConfigEntry<bool> _showTimer;
+        private static ConfigEntry<bool> _showDebugText;
         private static ConfigEntry<bool> _speedrunModeEnabled;
+        // start text
+        private static bool _showStartUpText = false;
 
-        const int MAX_STRING_SIZE = 499;
-
-        private static readonly GUIStyle _textStyle = new GUIStyle();
-        private static readonly GUIStyle _textShadowStyle = new GUIStyle();
-        private static readonly GUIStyle _debugTextStyle = new GUIStyle();
-        private static Rect _timerRect;
-        private static Rect _timerShadowRect;
-        private static Rect _debugMessageRect;
-        private static string _lastDebugMessage = "PLACEHOLDER";
-        private const int ScreenOffset = 10;
+        private static TextGUI _timerText = new TextGUI();
+        private static TextGUI _debugText = new TextGUI();
+        private static TextGUI _startupText = new TextGUI(TextAnchor.MiddleLeft, Color.white, $"strategineer's Pig Eat Ball Speedrun Tools version {PluginInfo.PLUGIN_VERSION} loaded.");
         private static bool isTimerOn = false;
 
         static void Log(string msg)
         {
             Console.WriteLine(msg);
-            _lastDebugMessage = msg;
+            _debugText.SetText(msg);
         }
 
         static void StartTimerIfNeeded(string reason)
@@ -126,8 +134,6 @@ namespace com.strategineer.PEBSpeedrunTools
                 StopTimerIfNeeded(__originalMethod.Name);
             }
 
-            // todo skip clam start chats and skip the leaderboard/award section
-
             [HarmonyPostfix]
             [HarmonyPatch(typeof(MenuClamTalkStart), nameof(MenuClamTalkStart.DoStartup))]
             static void PostfixSkipClamTalk(ref MenuClamTalkStart __instance)
@@ -135,30 +141,32 @@ namespace com.strategineer.PEBSpeedrunTools
                 if (_speedrunModeEnabled.Value)
                 {
                     Log($"Skipping clam talk");
-                    // Just skip the clam talk and play the level
+                    // Just skip talking to the clam and start playing the level right away
                     if (MidGame.staticMidGame.getCurrentMenu() is MenuClamTalkStart)
                     {
                         __instance.callFunctionDelayed(13);
                     }
-                    // todo bug: because of this change, when we go back into a completed level the timer pauses during the item select screen
                 }
             }
 
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(MenuWinScreen), nameof(MenuWinScreen.DoStartup))]
-            static void PostfixSkipWinScreen(ref MenuWinScreen __instance, ref int ___currentState, ref float ___currentStateTime)
+            static void PostfixSkipWinScreen(ref MenuWinScreen __instance, bool ___singlePlayerFailure, ref int ___currentState, ref float ___currentStateTime)
             {
                 if (_speedrunModeEnabled.Value)
                 {
                     Log("Skipping win screen");
-                    // Just skip the win screen and play the next level or return to the world screen
+                    // Just skip the win screen and play the next level or return to the world screen if we've beaten the level
+                    // Otherwise, let's restart the current level
                     ___currentStateTime = 100000f;
                     ___currentState = 42;
-                    __instance.callFunction(new GenericButtonActioner(0, 0));
+                    var stateNumber = ___singlePlayerFailure ? 1 : 0;
+                    // stateNumber 0 to move on, stateNumber 1 to restart the current level
+                    __instance.callFunction(new GenericButtonActioner(stateNumber, 0));
                 }
             }
-
+            /* todo see if this is needed
             [HarmonyPostfix]
             [HarmonyPatch(typeof(PigMenu), nameof(PigMenu.setSubMenu))]
             static void PostfixPigMenuSetSubMenu(ref PigMenu __instance, MenuBase ___currentSubMenu)
@@ -168,11 +176,11 @@ namespace com.strategineer.PEBSpeedrunTools
                     StartTimerIfNeeded("Disguises Menu entered");
                 }
             }
+            */
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(MenuBase), nameof(MenuBase.setCurrentButton))]
             // This starts the timer if we're on a pause menu and then we select a different button
-            // todo also figure out why the timer is paused when we enter into a clam level set
             static void PostfixMenuPauseSetCurrentButton(MenuBase __instance, ButtonBase ___lastButton, ButtonBase ___currentButton)
             {
                 if (__instance is MenuPause || __instance is MenuPauseInGame)
@@ -204,7 +212,11 @@ namespace com.strategineer.PEBSpeedrunTools
                     ResetTimer("Start game menu entered");
                 }
                 if(___currentMenu != null
+                    // this breaks our existing logic that skips these menus anyway
                     && ___currentMenu is not MenuClamTalkStart
+                    // we can use this to warp
+                    && ___currentMenu is not MenuPearls
+                    // this is the menu that shows up when we launch into a level?
                     && ___currentMenu is not LevelStartScreen)
                 {
                     StopTimerIfNeeded($"Menu/Dialog entered {___currentMenu}");
@@ -217,7 +229,6 @@ namespace com.strategineer.PEBSpeedrunTools
 
         private void Awake()
         {
-            InternalUnityLogger.OnUnityInternalLog += HandleUnityLog;
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
             _timerPosition = Config.Bind("Interface",
@@ -230,15 +241,23 @@ namespace com.strategineer.PEBSpeedrunTools
                 TextAnchor.LowerRight,
                 "Which corner of the screen to display the last debug message in.");
 
-            _showTimer = Config.Bind("Interface",
+            _showTimer = Config.Bind("Show/Hide",
                 "Show Timer",
                 true,
                 "Should show the speedrun timer?");
 
-            _speedrunModeEnabled = Config.Bind("Interface",
+            _showDebugText = Config.Bind("Show/Hide",
+             "Show Debug Text",
+             false,
+             "Should show the debug text?");
+
+            _speedrunModeEnabled = Config.Bind("Speedrun",
                 "Speedrun Mode Enabled",
                 true,
-                "Should we try to automatically skip any dialogs?");
+                "Should we try to automatically skip any menus/dialogs?");
+
+            _debugText.SetAnchor(_debugMsgPosition.Value);
+            _timerText.SetAnchor(_timerPosition.Value);
         }
 
         private void Start()
@@ -246,36 +265,9 @@ namespace com.strategineer.PEBSpeedrunTools
             h = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }
 
-        static void HandleUnityLog(object sender, UnityLogEventArgs e)
-        {
-            // do nothing but this might be useful later
-        }
-
-        private static void updateLooks()
+        private void OnGUI()
         {
 
-            _debugTextStyle.normal.textColor = _textStyle.normal.textColor = isTimerOn ? Color.yellow : Color.green;
-            _textShadowStyle.normal.textColor = Color.black;
-
-            int w = Screen.width, h = Screen.height;
-            _debugMessageRect = _timerRect = new Rect(ScreenOffset, ScreenOffset, w - ScreenOffset * 2, h - ScreenOffset * 2);
-            _timerShadowRect = _timerRect;
-            _timerShadowRect.x += 3;
-            _timerShadowRect.y += 3;
-
-
-            _textStyle.alignment = _textShadowStyle.alignment = _timerPosition.Value;
-            _debugTextStyle.alignment = _debugMsgPosition.Value;
-            _textStyle.fontSize = _textShadowStyle.fontSize = h / 40;
-        }
-
-        private void Update()
-        {
-            updateLooks();
-        }
-
-        private void displayTime()
-        {
             TimeSpan ts = stopWatch.Elapsed;
             string elapsedTime;
             if (ts.Hours > 0)
@@ -283,21 +275,27 @@ namespace com.strategineer.PEBSpeedrunTools
                 elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
                 ts.Hours, ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
-            } else
+            }
+            else
             {
                 elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
                 ts.Minutes, ts.Seconds,
                 ts.Milliseconds / 10);
             }
-            
-            GUI.Label(_timerShadowRect, elapsedTime, _textShadowStyle);
-            GUI.Label(_timerRect, elapsedTime, _textStyle);
-            GUI.Label(_debugMessageRect, _lastDebugMessage, _debugTextStyle);
-        }
 
-        private void OnGUI()
-        {
-            displayTime();
+            if (_showTimer.Value)
+            {
+                _timerText.SetText(elapsedTime);
+                _timerText.Draw();
+            }
+            if (_showDebugText.Value)
+            {
+                _debugText.Draw();
+            }
+            if (_showStartUpText)
+            {
+                _startupText.Draw();
+            }
         }
     }
 }
